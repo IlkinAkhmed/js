576a58e7edf9c59e6daa988ca6c8437e3a653b95
EminAbdulla <tu7exjg3e@code.edu.az>
2023-10-11 13:58:27 +0400

    js

---
+++ assets/index.js
@@ -0,0 +1,72 @@
+// for (let i=1; i<=50;i++){
+//     console.log(i);
+// }
+
+
+
+// let a = 1
+// while (a<=50){
+//     a++;
+//     console.log(a);
+// }
+
+// let a = 1
+// cem = 0
+// while (a<=100){
+//     a++;
+//     cem+=a;
+// }
+//     console.log(cem);
+
+
+
+// let a = 1
+// hasil = 1
+// while (a<=10){
+//     a++;
+//     hasil*=a;
+// }
+//     console.log(hasil);
+
+
+
+// a = 7
+// while(a<=170){
+//     a++;
+//     if (a%27===0){
+//         console.log(a);
+//     }
+// }
+
+
+// let a = ['salam',4,6,'ilkin']
+// console.log(a[0], a[1]);
+
+
+
+// const arr=['ilkin','ehmedov','ucar',76,100]
+// for (let index = 0; index < arr.length; index++) {
+//     console.log(arr[index]);
+    
+// }
+
+// const arr=['ilkin','ehmedov','ucar',76,100]
+// for (let index = 0; index < arr.length; index++) {
+//     if(arr[index]==='ilkin')
+//     console.log(arr[index]);
+
+// }
+
+
+// let a='12345678901'
+// for (let index = 0; index < a.length; index++) {
+//     console.log(a[index]);
+    
+// }
+
+
+
+
+
+
+
